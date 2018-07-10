82b0ad5b8b6c0c0aefbdae94ba52f43ed913b93f
Pantelis Andrianakis <pantelisandrianakis@gmail.com>
2018-07-10 04:08:50 +0300

    Dropped C3P0 in favor of HikariCP.

--- java/com/epicdragonworld/gameserver/managers/DatabaseManager.java
+++ java/com/epicdragonworld/gameserver/managers/DatabaseManager.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.util.logging.Logger;
 
 import com.epicdragonworld.Config;
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * @author Pantelis Andrianakis
@@ -13,35 +13,21 @@ public class DatabaseManager
 {
 	private static final Logger LOGGER = Logger.getLogger(DatabaseManager.class.getName());
 	
-	private static final ComboPooledDataSource _cpds = new ComboPooledDataSource();
+	private static final HikariDataSource _hds = new HikariDataSource();
 	
 	public DatabaseManager()
 	{
-		try
-		{
-			_cpds.setDriverClass(Config.DATABASE_DRIVER);
-			_cpds.setJdbcUrl(Config.DATABASE_URL);
-			_cpds.setUser(Config.DATABASE_LOGIN);
-			_cpds.setPassword(Config.DATABASE_PASSWORD);
-			_cpds.setInitialPoolSize(2);
-			_cpds.setMinPoolSize(2);
-			_cpds.setMaxPoolSize(Math.max(2, Config.DATABASE_MAX_CONNECTIONS));
-			_cpds.setAcquireIncrement(5);
-			_cpds.setAcquireRetryDelay(500);
-			_cpds.setCheckoutTimeout(0);
-			_cpds.setMaxIdleTime(Config.DATABASE_MAX_IDLE_TIME);
-			_cpds.setBreakAfterAcquireFailure(false);
-			_cpds.setAutoCommitOnClose(true);
-		}
-		catch (Exception e)
-		{
-			LOGGER.severe("DatabaseManager: Problem initializing connection. " + e);
-		}
+		_hds.setDriverClassName(Config.DATABASE_DRIVER);
+		_hds.setJdbcUrl(Config.DATABASE_URL);
+		_hds.setUsername(Config.DATABASE_LOGIN);
+		_hds.setPassword(Config.DATABASE_PASSWORD);
+		_hds.setMaximumPoolSize(Config.DATABASE_MAX_CONNECTIONS);
+		_hds.setIdleTimeout(Config.DATABASE_MAX_IDLE_TIME);
 		
 		// Test if connection is valid.
 		try
 		{
-			_cpds.getConnection().close();
+			_hds.getConnection().close();
 		}
 		catch (Exception e)
 		{
@@ -53,7 +39,7 @@ public class DatabaseManager
 	{
 		try
 		{
-			return _cpds.getConnection();
+			return _hds.getConnection();
 		}
 		catch (Exception e)
 		{
@@ -66,7 +52,7 @@ public class DatabaseManager
 	{
 		try
 		{
-			_cpds.close();
+			_hds.close();
 		}
 		catch (Exception e)
 		{
