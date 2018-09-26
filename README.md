0f8b3f60caedd4317f69ad94e107ec6688b7ef50
Pantelis Andrianakis <pantelisandrianakis@gmail.com>
2018-09-26 09:14:32 +0300

    Chat and messaging changes.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Java/Netty server that connects with Unity3D/C# clients.
 - Client authentication.
 - Character management.
 - Broadcast game object movement.
+- Chat and messaging.
 
 ### You can see the latest development progress on YouTube.
 [![Video demonstration](https://img.youtube.com/vi/I5hA0cYZCoE/0.jpg)](https://www.youtube.com/watch?v=I5hA0cYZCoE&list=PLNuit1aMUWTDRll1MGF7Cqn_lX-BqKpZn)
--- java/com/epicdragonworld/gameserver/managers/ChatManager.java
+++ java/com/epicdragonworld/gameserver/managers/ChatManager.java
@@ -14,6 +14,8 @@ public class ChatManager
 	private static final byte CHAT_TYPE_MESSAGE = 2;
 	private static final String COMMAND_PERSONAL_MESSAGE = "/tell ";
 	private static final String COMMAND_LOCATION = "/loc";
+	private static final String SYS_NAME = "System";
+	private static final String MSG_TO = "To ";
 	
 	public static void handleChat(Player sender, String message)
 	{
@@ -24,37 +26,38 @@ public class ChatManager
 			return;
 		}
 		
-		final String lowercaseMessage = message.toLowerCase().replaceAll("\\s+", ""); // Also remove all double spaces.
-		
+		final String lowercaseMessage = message.toLowerCase().replaceAll("\\s{2,}", " "); // Also remove all double spaces.
 		if (lowercaseMessage.equals(COMMAND_LOCATION))
 		{
 			final Location location = sender.getLocation();
-			sender.channelSend(new ChatResult(CHAT_TYPE_SYSTEM, "", "Your location is " + location.getX() + " " + location.getY() + " " + location.getZ()));
+			sender.channelSend(new ChatResult(CHAT_TYPE_SYSTEM, SYS_NAME, "Your location is " + location.getX() + " " + location.getY() + " " + location.getZ()));
 		}
 		else if (lowercaseMessage.startsWith(COMMAND_PERSONAL_MESSAGE))
 		{
 			final String[] lowercaseMessageSplit = lowercaseMessage.split(" ");
 			if (lowercaseMessageSplit.length < 3) // Check for parameters.
 			{
-				sender.channelSend(new ChatResult(CHAT_TYPE_SYSTEM, "", "Incorrect syntax. Use /tell [name] [message]."));
+				sender.channelSend(new ChatResult(CHAT_TYPE_SYSTEM, SYS_NAME, "Incorrect syntax. Use /tell [name] [message]."));
 				return;
 			}
 			
 			final Player receiver = WorldManager.getPlayerByName(lowercaseMessageSplit[1]);
 			if (receiver == null)
 			{
-				sender.channelSend(new ChatResult(CHAT_TYPE_SYSTEM, "", "Player was not found."));
+				sender.channelSend(new ChatResult(CHAT_TYPE_SYSTEM, SYS_NAME, "Player was not found."));
 			}
 			else
 			{
 				// Step by step cleanup, to avoid problems with extra/double spaces on original message.
 				message = message.substring(lowercaseMessageSplit[0].length(), message.length()).trim(); // Remove command.
 				message = message.substring(lowercaseMessageSplit[1].length(), message.length()).trim(); // Remove receiver name.
+				sender.channelSend(new ChatResult(CHAT_TYPE_MESSAGE, MSG_TO + receiver.getName(), message));
 				receiver.channelSend(new ChatResult(CHAT_TYPE_MESSAGE, sender.getName(), message));
 			}
 		}
 		else // Normal message.
 		{
+			sender.channelSend(new ChatResult(CHAT_TYPE_NORMAL, sender.getName(), message));
 			for (Player player : WorldManager.getVisiblePlayers(sender))
 			{
 				player.channelSend(new ChatResult(CHAT_TYPE_NORMAL, sender.getName(), message));
